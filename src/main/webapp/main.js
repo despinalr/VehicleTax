903bf306e931a298c342d711368bbc5ee7299dd7
David Espinal Ruiz <davidesr7@gmail.com>
2014-05-05 15:52:03 -0500

    Parte 1 App

---
+++ src/main/webapp/main.js
@@ -0,0 +1,9 @@
+$(document).on("ready", function() {
+	$("#consultar").click(function() {
+            alert("Consultar");
+	});
+        
+        $("#actualizar").click(function() {
+            alert("Actualizar");
+	});
+});
